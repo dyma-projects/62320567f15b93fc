9998d949782dbff196b5dbde1c70884203c77b12
django123 <edougajean@gmail.com>
2022-04-27 12:37:35 +0200

    exercice chapitre 7,8,9

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  public users$: BehaviorSubject<string[]> = new BehaviorSubject([]);
+
+  public addUser(username: string): void {
+    this.users$.value[username];
+  }
+
+  constructor() { }
+}
